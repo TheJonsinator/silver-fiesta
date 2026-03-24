a57dafed29f3000f60f9ea5e5d36f3b41455e5b6
TheJonsinator <jonastaulenordli@gmail.com>
2026-03-24 12:09:47 +0000

    jjj

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,66 @@ import Gameboard from "./Gameboard.jsx";
 import { useState } from 'react';
 import {useQuery} from "@tanstack/react-query";
 
+
 function App() {
   //All My Consts
-  const cardValues=[1,2,3,4,5];
-  const [correctAnswer,setCorrectAnswer]=useState([]);
-  const listOfGuesses=[1,2,3,4,5]
-  const [allSpells,setAllSpells]=useState([])
+  const cardValues=[1,2,3,4,5,6,7];
+  const [correctAnswer,setCorrectAnswer]=useState(Math.floor(Math.random()*320));
+  const [listOfGuesses,setListOfGuesses]=useState([])
+  
 
-  //This entire thing, is just to get the query, but I imagine that after I get it 1 time, I dont need to load it more
+
+ 
+   
+
+
+  //This entire thing, is just to get the query with the insex and names, but I imagine that after I get it 1 time, I dont need to load it more
   const {isPending,error,data} = useQuery({
       queryKey:["spellData"],
       queryFn:()=>
-        fetch("https://www.dnd5eapi.co/api/2014/spells").then((res)=>res.json(),),
+        fetch("https://www.dnd5eapi.co/api/2014/spells/").then((res)=>res.json(),),
     })
     if(isPending) return 'Loading'
     if(error) return "an error occured:"+error.message
-    console.log(data.results)
-  
+    
+   
+    const correctName=data.results[correctAnswer].index
+
+
+
+     const {data:metaData} = useQuery({
+      queryKey:["metaData",correctName],
+      queryFn:()=>
+        fetch(`https://www.dnd5eapi.co/api/2014/spells/${correctName}`).then((res)=>res.json(),),
+    })
+   
+    
+
+
+
+    //Using addItem causes an infinite loop.
+const addItem = async (term) => {
+      const res = await fetch(`https://www.dnd5eapi.co/api/2014/spells/${term}`);
+      const data = await res.json();
+      setListOfGuesses(prev => [...prev, data]);
+  };
+ 
+ 
+  //Randomly Choses a number from between one and 320(the amount of spells are 319,I hardcoded this cause while I could you length of the resultList)
+    console.log(correctAnswer)
+  console.log(listOfGuesses)
+  console.log(data.results[correctAnswer].index)
+  console.log(metaData)
+
+
+   
+
+
+    
+  //Læringsnotat, du kan ikke bruke useState inni en funksjon
   //--------------------------------Sliter med å gjøre den under til en custom api get call, for sånn det er nå,
 
-    function useSpell(spellName){
-      return useSuspenseQuery({
-      queryKey:["spell",spellName],
-      queryFn:({queryKey:[, spellName]})=>{
-        fetch(`https://www.dnd5eapi.co/api/2014/spells/${spellName}`).then(response=> response.json());
-      }});}
-
+  
 
 
 

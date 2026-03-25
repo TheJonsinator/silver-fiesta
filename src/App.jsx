69be58b88f3b84c043ac26c938bd9aaaf5a458f8
TheJonsinator <jonastaulenordli@gmail.com>
2026-03-25 08:37:25 +0000

    Yes

--- src/App.jsx
+++ src/App.jsx
@@ -8,62 +8,29 @@ function App() {
   //All My Consts
   const cardValues=[1,2,3,4,5,6,7];
   const [correctAnswer,setCorrectAnswer]=useState(Math.floor(Math.random()*320));
+  const [selectedSpellIds,setSelectedSpellIds]=useState([])
   const [listOfGuesses,setListOfGuesses]=useState([])
   
 
 
- 
+
    
 
 
   //This entire thing, is just to get the query with the insex and names, but I imagine that after I get it 1 time, I dont need to load it more
-  const {isPending,error,data} = useQuery({
-      queryKey:["spellData"],
+  const {isPending,error,data:spellNames} = useQuery({
+      queryKey:["spellNames"],
       queryFn:()=>
         fetch("https://www.dnd5eapi.co/api/2014/spells/").then((res)=>res.json(),),
     })
     if(isPending) return 'Loading'
     if(error) return "an error occured:"+error.message
-    
-   
-    const correctName=data.results[correctAnswer].index
 
-
-
-     const {data:metaData} = useQuery({
-      queryKey:["metaData",correctName],
-      queryFn:()=>
-        fetch(`https://www.dnd5eapi.co/api/2014/spells/${correctName}`).then((res)=>res.json(),),
-    })
-   
-    
-
-
-
-    //Using addItem causes an infinite loop.
-const addItem = async (term) => {
-      const res = await fetch(`https://www.dnd5eapi.co/api/2014/spells/${term}`);
-      const data = await res.json();
-      setListOfGuesses(prev => [...prev, data]);
-  };
- 
- 
   //Randomly Choses a number from between one and 320(the amount of spells are 319,I hardcoded this cause while I could you length of the resultList)
-    console.log(correctAnswer)
-  console.log(listOfGuesses)
-  console.log(data.results[correctAnswer].index)
-  console.log(metaData)
-
-
-   
-
-
-    
+  console.log(correctAnswer)
+  console.log(spellNames.results[correctAnswer].index)    
   //Læringsnotat, du kan ikke bruke useState inni en funksjon
   //--------------------------------Sliter med å gjøre den under til en custom api get call, for sånn det er nå,
-
-  
-
 
 
   return (
--- src/Gameboard.jsx
+++ src/Gameboard.jsx
@@ -1,7 +1,9 @@
 import "./Gameboard.css";
 import GameCard from "./GameCard.jsx"
+import {useState} from "react"
 
-export default function GameBoard({cardValues}){
+export default function GameBoard({spellName}){
+    const [cardValues,setCardValues]=useState([1,2,3])
   
     return(<>
     <div className="Gameboard">

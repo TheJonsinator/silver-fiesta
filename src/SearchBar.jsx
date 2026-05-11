6204c4834a0832e73a81b0cecc3818132a64192e
TheJonsinator <jonastaulenordli@gmail.com>
2026-05-11 06:32:41 +0000

    Speldle

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -3,7 +3,7 @@ import "./SearchBar.css"
 export default function SearchBar({submitFunction}){
     return <>
     <form onSubmit={submitFunction}>
-        <label htmlFor="spellname">SPELLDLE</label><br></br>
+        <label htmlFor="spellname">SPELDLE</label><br></br>
         <input type="text" name= "spellName" id="spellname"/>
     </form>
     
